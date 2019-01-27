f1f8da25bb54fc054b6e48762a43c7c1867bea7b
Igorkh106 <igorkh106@gmail.com>
2019-01-27 19:45:53 +0200

    Elevators cleanup

--- src/app/elevators/elevators.component.ts
+++ src/app/elevators/elevators.component.ts
@@ -1,25 +1,19 @@
-import {Component, OnInit} from '@angular/core';
-import {ElevatorsService} from '../services/elevators.service';
-import AppConstants from '../constants';
-import {select, Store} from '@ngrx/store';
-import {Observable} from 'rxjs/internal/Observable';
-import {selectAll, selectAllElevators, selectElevatorsIDs, selectElevatorsTotal} from 'app/store';
-import {distinctUntilChanged, filter, map, mergeMap, tap} from 'rxjs/operators';
+import { Component, OnInit } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs/internal/Observable';
+import { selectElevatorsIDs } from 'app/store';
+import { distinctUntilChanged, tap } from 'rxjs/operators';
 
 @Component({
-    selector: 'elevators',
+    selector: 'app-elevators',
     templateUrl: './elevators.component.html',
-    styleUrls: ['./elevators.component.scss'],
-    providers: [ElevatorsService]
+    styleUrls: ['./elevators.component.scss']
 })
 
 export class ElevatorsComponent implements OnInit {
     elevators$: Observable<any>;
-    elevatorsJson: any[];
-    elevatorsServiceEvents: any;
 
-    constructor(protected elevatorsService: ElevatorsService,
-                private store: Store<any>) {
+    constructor(private store: Store<any>) {
     }
 
     ngOnInit() {
@@ -29,25 +23,5 @@ export class ElevatorsComponent implements OnInit {
                 distinctUntilChanged(),
                 tap(console.log)
             );
-
-        this.elevatorsJson = this.elevatorsService.getElevatorsJson();
     }
-
-    updateElevatorsList() {
-
-    }
-
-    onElevatorsChanged(elvatorsJson) {
-        this.elevatorsJson = elvatorsJson;
-    }
-
-    onTaskArrivedToDest() {
-        this._playDing();
-    }
-
-    _playDing() {
-        let audio = new Audio(AppConstants.ASSETS_URL + 'ding.mp3');
-        audio.play();
-    }
-
 }
